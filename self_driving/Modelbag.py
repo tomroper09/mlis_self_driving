3010b30ae81a83024748bf089640c1baeb023462
JinhaoFang <148857590+JinhaoFang@users.noreply.github.com>
2024-05-02 17:01:16 +0100

    Update Modelbag.py

--- self_driving/Modelbag.py
+++ self_driving/Modelbag.py
@@ -1,58 +1,3 @@
-import pandas as pd
-import os
-import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, History
-from sklearn.model_selection import train_test_split,KFold
-import time
-import csv
-from datetime import datetime
-
-log_path="/home/alyjf10/self_driving_car/logs/fit/"
-loss_img='/home/alyjf10/self_driving_car/loss_plot.png'
-output_file = '/home/alyjf10/self_driving_car/model/Kfold/'
-
-class PilotNet(tf.keras.Model):
-    def __init__(self, input_shape=(224, 224, 3)):
-        super(PilotNet, self).__init__()
-        self.conv1 = tf.keras.layers.Conv2D(24, (5, 5), strides=(2, 2), activation='relu', input_shape=input_shape)
-        self.conv2 = tf.keras.layers.Conv2D(36, (3, 3), strides=(2, 2), activation='relu')
-        self.conv3 = tf.keras.layers.Conv2D(48, (3, 3), strides=(2, 2), activation='relu')
-        self.conv4 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')
-        self.conv5 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')
-        self.flatten = tf.keras.layers.Flatten()
-        self.fc1 = tf.keras.layers.Dense(100, activation='relu')
-        self.fc2 = tf.keras.layers.Dense(50, activation='relu')
-        self.fc3 = tf.keras.layers.Dense(10, activation='relu')
-        self.fc4 = tf.keras.layers.Dense(1, activation='linear')
-
-    def call(self, inputs):
-        x = self.conv1(inputs)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = self.fc3(x)
-        output = self.fc4(x)
-        return output
-class SENet(tf.keras.Model):
-    def __init__(self, channels, reduction_ratio=16):
-        super(SENet, self).__init__()
-        self.global_pooling = tf.keras.layers.GlobalAveragePooling2D()
-        self.fc1 = tf.keras.layers.Dense(channels // reduction_ratio, activation='relu')
-        self.fc2 = tf.keras.layers.Dense(channels, activation='sigmoid')
-
-    def call(self, inputs):
-        x = self.global_pooling(inputs)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = tf.expand_dims(tf.expand_dims(x, axis=1), axis=1)
-        return x
-
 class NN(tf.keras.Model):
     def __init__(self,model_name):
         '''
@@ -221,120 +166,96 @@ class NN(tf.keras.Model):
         print("Data Process!")
         return image
 
-    def load_data(self, root_path, img_path, csv_path,flod_number ):
+    def load_data(self, root_path, img_path, csv_path,batch_size=16):
         # load csv
-        train_img = []
-        train_label = []
-        val_img = []
-        val_label = []
         labels_df = pd.read_csv(os.path.join(root_path, csv_path))
 
         # path process
         labels_df['image_path'] = labels_df['image_id'].apply(lambda x: os.path.join(root_path, img_path, f"{x}.png"))
         labels_df = labels_df[labels_df['image_path'].apply(os.path.exists)]
-        print('Amount of path: ',len(labels_df))
         # split into train dataset and validation dataset
 
-        # Split data into two parts based on index
-        #flip_data = labels_df[labels_df.index > 13794]
-        #train_df, val_df = train_test_split(original_datasets, test_size=0.2, random_state=6)
-
-
-        kfold = KFold(n_splits=flod_number, shuffle=True, random_state=42)
-        for train_indices, val_indices in kfold.split(labels_df):
-            train_df = labels_df.iloc[train_indices]
-            val_df = labels_df.iloc[val_indices]
-            # Combine remaining data with training set
-            # to tensor type
-            train_img_dataset = tf.data.Dataset.from_tensor_slices((
-                train_df['image_path'].values
-            ))
-            val_img_dataset = tf.data.Dataset.from_tensor_slices((
-                val_df['image_path'].values
-            ))
-            train_label_dataset = tf.data.Dataset.from_tensor_slices((
-                train_df['angle'].values,
-                train_df['speed'].values
-            ))
-            val_label_dataset = tf.data.Dataset.from_tensor_slices((
-                val_df['angle'].values,
-                val_df['speed'].values
-            ))
-
-            train_img.append(train_img_dataset)
-            train_label.append(train_label_dataset)
-            val_img.append(val_img_dataset)
-            val_label.append(val_label_dataset)
-
-
-        return train_img,train_label,val_img,val_label
-
-
-
+            # Split data into two parts based on index
+        original_datasets = labels_df[labels_df.index <= 13794]
+        flip_data = labels_df[labels_df.index > 13794]
+        train_df, val_df = train_test_split(original_datasets, test_size=0.2, random_state=6)
+        # Combine remaining data with training set
+        train_df = pd.concat([train_df, flip_data])
+        # to tensor type
+        train_img_dataset = tf.data.Dataset.from_tensor_slices((
+            train_df['image_path'].values
+        ))
+        val_img_dataset = tf.data.Dataset.from_tensor_slices((
+            val_df['image_path'].values
+        ))
+        train_label_dataset = tf.data.Dataset.from_tensor_slices((
+            train_df['angle'].values,
+            train_df['speed'].values
+        ))
+        val_label_dataset = tf.data.Dataset.from_tensor_slices((
+            val_df['angle'].values,
+            val_df['speed'].values
+        ))
 
         
+        train_img_dataset = train_img_dataset.map(lambda x: self.preprocess_image(x, augment=True),
+                                        num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        
+        val_img_dataset = val_img_dataset.map(lambda x: self.preprocess_image(x, augment=False),
+                                    num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        #zip img and label
+        train_dataset = tf.data.Dataset.zip((train_img_dataset,train_label_dataset))
+        val_dataset = tf.data.Dataset.zip((val_img_dataset, val_label_dataset))
+        train_dataset = train_dataset.shuffle(buffer_size=len(train_df)*2)
+        return train_dataset.batch(batch_size), val_dataset.batch(batch_size)
 
-
-    def training(self,train_img,train_label,val_img,val_label, epochs,batch_size, trained_model,model_save_path):
+    def training(self, train_dataset, val_dataset, epochs, trained_model,model_save_path):
         if trained_model != None:
             self.load_weights(trained_model)        
         self.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-            loss={'output_1': 'mse', 'output_2': 'mse'},#binary_crossentropy
+            loss={'output_1': 'mse', 'output_2': 'mse'},
             metrics=['mse'])        
         self.build([None, 224, 224, 3])
-
+        
         log_dir = log_path + datetime.now().strftime("%Y%m%d-%H%M%S")
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
         checkpoint_callback = ModelCheckpoint(filepath=model_save_path, monitor='val_loss',save_best_only=True)
-        earlystopping_callback = EarlyStopping(patience=10,restore_best_weights=True) 
+        earlystopping_callback = EarlyStopping(patience=15,restore_best_weights=True) 
         history_callback = History()
-        fold_metrics = []  # 用于存储每个 fold 的指标结果
+        start = time.time()
+        history = self.fit(train_dataset,
+                        epochs=epochs,
+                        validation_data=val_dataset,
+                        callbacks=[checkpoint_callback, tensorboard_callback, earlystopping_callback,history_callback])
 
-        for i in range(len(train_img)):
-            print(f"**********************************************************************FOLD{i+1}**********************************************************************************")
-            train_img_dataset = train_img[i]
-            train_label_dataset = train_label[i]
-            val_img_dataset = val_img[i]
-            val_label_dataset = val_label[i]
-
-            train_img_dataset = train_img_dataset.map(lambda x: self.preprocess_image(x, augment=True),
-                                 num_parallel_calls=tf.data.experimental.AUTOTUNE)
-            val_img_dataset = val_img_dataset.map(lambda x: self.preprocess_image(x, augment=False),
-                                        num_parallel_calls=tf.data.experimental.AUTOTUNE)
-            #zip img and label
-            train_dataset = tf.data.Dataset.zip((train_img_dataset,train_label_dataset))
-            val_dataset = tf.data.Dataset.zip((val_img_dataset, val_label_dataset))
-            train_dataset = train_dataset.shuffle(buffer_size=len(train_img_dataset)*2) 
-            train_dataset=train_dataset.batch(batch_size)
-            val_dataset=val_dataset.batch(batch_size)
-
-
-            start = time.time()
-
-
-            self.fit(train_dataset,
-                            epochs=epochs,
-                            validation_data=val_dataset,
-                            validation_freq = 2,
-                            callbacks=[checkpoint_callback, tensorboard_callback, earlystopping_callback,history_callback])
-
-            val_loss, output_1_loss, output_2_loss, output_1_mse, output_2_mse = self.evaluate(val_dataset)
-
-            fold_metrics.append({'val_mse': val_loss,
-                             'Angle': output_1_mse,
-                             'Speed': output_2_mse})
-            
-
-            end = time.time()
-            #self.save(model_save_path, save_format='tf')
-            print('Time: {:.2f} minutes'.format((end - start) / 60))
-            print(f"Fold {i+1} Metrics -  Validation MSE: val_mse:{val_loss},angle:{output_1_mse},speed:{output_2_mse}")
-
+        end = time.time()
+        self.save(model_save_path, save_format='tf')
+        print('Time: {:.2f} minutes'.format((end - start) / 60))
         
+        plt.figure(figsize=(12, 8))
+        plt.subplot(2, 1, 1)
+        plt.plot(history.history['output_1_loss'], label='Training Angle Loss')
+        plt.plot(history.history['val_output_1_loss'], label='Validation Angle Loss')
+        plt.title('Angle Loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+
+        plt.subplot(2, 1, 2)
+        plt.plot(history.history['output_2_loss'], label='Training Speed Loss')
+        plt.plot(history.history['val_output_2_loss'], label='Validation Speed Loss')
+        plt.title('Speed Loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+
+        plt.tight_layout()
+        plt.savefig(loss_img)
+        return history
 
     def predict_model(self, trained_model_path, image_path, output_path):
         # 加载已经训练好的模型
-        self.load_weights(trained_model_path)
+        
         label_interval = 0.0625
         num_labels = 17
         # 标签的最小值
@@ -342,7 +263,7 @@ class NN(tf.keras.Model):
         
         # 预测值所在的标签范围
         label_range = [min_label + i * label_interval for i in range(num_labels)]
-
+        self.load_weights(trained_model_path)
         # 打开 CSV 文件，准备写入数据
         with open(output_path, 'w', newline='') as csvfile:
             # 定义 CSV 写入器
@@ -374,9 +295,16 @@ class NN(tf.keras.Model):
                 else:
                     speed = speed
 
+                if angle >= 1:
+                    angle = 1
+                elif angle <= 0:
+                    angle = 0
+                else:
+                    angle = angle
+
 
                 # 计算预测值与每个标签的差值的绝对值
-                #differences = [abs(prediction - label) for label in label_range]
+                #differences = [abs(angle - label) for label in label_range]
                 #
                 ## 找到最小差值对应的标签值
                 #predicted_label_value = label_range[differences.index(min(differences))]
@@ -384,19 +312,9 @@ class NN(tf.keras.Model):
 
                 
                 # 四舍五入到合适的精度
-                angle = np.round(angle, 6)
-                speed = np.round(speed, 6)
+                angle = np.round(angle, 4)
+                speed = np.round(speed, 4)
                 image_id = image_name.split('.')[0]
 
                 writer.writerow([image_id, angle, speed])
         print('Finish!')
-
-
-
-
-
-
-
-        
- 
- 
